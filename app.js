401576887fa90b1bc54f007a0c11f0dbc7375b5c
tombetthauser <tombetthauser@gmail.com>
2021-02-28 18:46:17 -0800

    Commented out some links temporarily

--- app.js
+++ app.js
@@ -6,18 +6,16 @@ const things = {
         }]
     },{ tag: `p`,
       className: `image-text`,
-      innerText: `Sacramento Collage, 8.5 x 11 in. original drawings and paintings on photocopies of original drawings and paintings, 2021`
+      innerText: `Office Collage, 8.5 x 11 in. original drawings and paintings on photocopies of original drawings and paintings, 2021`
     },{ tag: `h1`,
       innerText: `Tom Betthauser`
     },{ tag: `h2`,
       innerText: `Visual Artist / Software Engineer`
-    // },{ tag: `h3`,
-    //   innerText: `Yale School of Art, MFA 2012`
     },{ tag: `hr`,
     },{ tag: `p`,
-      innerText: `Hello, I'm a multimedia artist and software developer currently working as an educator and maintaining my studio practice as an ongoing resident at the Verge Center for the Arts in Sacramento, CA. I'm making a futile effort to get off social media by focusing my efforts on an old-school email newsletter, a blog, art zines and pop-up exhibitions with other artists.`
+      innerText: `Hello, I'm a multimedia artist and software developer currently working as an educator and maintaining my studio practice as an ongoing resident at the Verge Center for the Arts in Sacramento, CA.`
     },{ tag: `p`,
-      innerText: `I'm always up for studio visits and collaboration oppourtunities with other artists, tech people, and artists masquerading as tech people (like me) so please don't hesitate to drop me a regular old email any time.`
+      innerText: `I'm always up for studio visits and collaboration opportunities with other artists, tech people, and artists masquerading as tech people (like me) so please don't hesitate to drop me a regular old email any time.`
     },{ tag: `p`,
       innerText: `tombetthauser@gmail.com`
     },{ tag: `hr`
@@ -25,33 +23,36 @@ const things = {
       innerText: `Some me-related links...`
     },{ tag: `ul`,
       babies: [{ tag: `li`, babies: [{ tag: `a`,
-              innerText: `my email newsletter thing`,
-              href: `https://docs.google.com/forms/d/e/1FAIpQLSeUSTIFKk7IitfyXX7EeCuyEGOX8n1FqZ5SMbLVNXDAtTQ8yQ/viewform?usp=sf_link`
+            //   innerText: `my email newsletter thing`,
+            //   href: `https://docs.google.com/forms/d/e/1FAIpQLSeUSTIFKk7IitfyXX7EeCuyEGOX8n1FqZ5SMbLVNXDAtTQ8yQ/viewform?usp=sf_link`
+            // }]},{ tag: `li`, babies: [{ tag: `a`,
+              innerText: `my github page for computer people`,
+              href: `https://github.com/tombetthauser`
+            }]},{ tag: `li`, babies: [{ tag: `a`,
+              innerText: `my instagram which I hate and barely use`,
+              href: `https://www.instagram.com/tombetthauser/`
             }]},{ tag: `li`, babies: [{ tag: `a`,
               innerText: `my random bandcamp page`,
               href: `https://tombetthauser.bandcamp.com/`
             }]},{ tag: `li`, babies: [{ tag: `a`,
-              innerText: `my art merch shop for buying stuff`,
-              href: `https://www.etsy.com/shop/tombetthauser`
-            }]},{ tag: `li`, babies: [{ tag: `a`,
-              innerText: `my github page for computer people`,
-              href: `https://github.com/tombetthauser`
-            }]},{ tag: `li`, babies: [{ tag: `a`,
+            //   innerText: `my art merch shop for buying stuff`,
+            //   href: `https://www.etsy.com/shop/tombetthauser`
+            // }]},{ tag: `li`, babies: [{ tag: `a`,
               innerText: `my linkedin page for whatever`,
               href: `https://www.linkedin.com/in/tombetthauser/`
             }]}]
     },{ tag: `p`,
-      innerText: `Some recent projects...`
+      innerText: `Some of my recent projects...`
     },{ tag: `ul`,
       babies: [{ tag: `li`, babies: [{ tag: `a`,
-              innerText: `hypernormalisation ipsum generator`,
-              href: `https://tombetthauser.github.io/hypernormalisipsum/`
-            }]},{ tag: `li`, babies: [{ tag: `a`,
               innerText: `state of the arts survey data project`,
               href: `http://www.sotasurvey.org/2019`
             }]},{ tag: `li`, babies: [{ tag: `a`,
-              innerText: `unknowablesymbols web meditations`,
+              innerText: `unknowable symbols web meditations`,
               href: `http://unknowablesymbols.com`
+            }]},{ tag: `li`, babies: [{ tag: `a`,
+              innerText: `hypernormalisation ipsum generator`,
+              href: `https://tombetthauser.github.io/hypernormalisipsum/`
             }]},{ tag: `li`, babies: [{ tag: `a`,
               innerText: `tomblr social media clone`,
               href: `http://tomblr.herokuapp.com/`
