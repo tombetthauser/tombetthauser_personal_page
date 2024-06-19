c03b7739a150488e4b7618564976fd5a259935d1
tombetthauser <tombetthauser@gmail.com>
2024-06-19 13:49:53 -0700

    automation script

--- update.py
+++ update.py
@@ -1,41 +1,54 @@
 import os
 from datetime import datetime
+from PIL import Image
+from PIL.ExifTags import TAGS
 
-# Define the path to the images folder and the index.html file
 images_folder = 'feed'
-index_file = './index.html'
+# index_file = './index.html'
+
+def get_exif_date(image_path):
+    try:
+        image = Image.open(image_path)
+        exif_data = image._getexif()
+        if exif_data:
+            for tag, value in exif_data.items():
+                tag_name = TAGS.get(tag, tag)
+                if tag_name == 'DateTimeOriginal':
+                    return datetime.strptime(value, '%Y:%m:%d %H:%M:%S')
+    except Exception as e:
+        print(f"Error getting EXIF date for {image_path}: {e}")
+    return None
 
 def update_file(images_folder, index_file):
-    # Get a list of image files in the folder, sorted by modification time in reverse order
-    image_files = sorted(
-        [f for f in os.listdir(images_folder) if f.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))],
-        key=lambda x: os.path.getctime(os.path.join(images_folder, x)),
-        reverse=True
-    )
+    image_files = [f for f in os.listdir(images_folder) if f.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))]
 
-    # Generate the HTML content for the images
-    image_html = ""
+    image_files_with_dates = []
     for image_file in image_files:
+        image_path = os.path.join(images_folder, image_file)
+        exif_date = get_exif_date(image_path)
+        if exif_date is None:
+            exif_date = datetime.fromtimestamp(os.path.getctime(image_path))
+        image_files_with_dates.append((image_file, exif_date))
+
+    sorted_image_files = sorted(image_files_with_dates, key=lambda x: x[1], reverse=True)
+
+    image_html = ""
+    for image_file, creation_time in sorted_image_files:
         img_src = os.path.join(images_folder, image_file)
         filename = os.path.splitext(image_file)[0].replace('-', ' ')
-        creation_time = datetime.fromtimestamp(os.path.getctime(os.path.join(images_folder, image_file)))
-        # formatted_date = creation_time.strftime('%Y-%m-%d %H:%M:%S')
         formatted_date = creation_time.strftime('%m/%d/%y')
-        # image_html += f'<img class="feed-image" src="{img_src}">\n<p class="feed-text">{img_src} [{formatted_date}]</p>\n'
-        # image_html += f'<img class="feed-image" src="{img_src}">\n<p class="feed-text"><span class="heart-emoji">♥️</span> {image_file}</p>\n'
-        image_html += f'<img class="feed-image" src="{img_src}">\n<p class="feed-text">{image_file}</p>\n'
+        # image_html += f'<img class="feed-image" src="{img_src}">\n<p class="feed-text">{filename} - {formatted_date}</p>\n'
+        image_html += f'<img class="feed-image" src="{img_src}">\n<p class="feed-text">[{formatted_date}]</p>\n'
 
-    # Read the existing index.html file
+
     with open(index_file, 'r') as file:
         html_content = file.read()
 
-    # Find the start and end of the image container div
     start_marker = '<div id="image-container">'
     end_marker = '</div>'
     start_index = html_content.find(start_marker)
     end_index = html_content.find(end_marker, start_index)
 
-    # If the div is found, replace its content; otherwise, create a new div
     if start_index != -1 and end_index != -1:
         new_html_content = html_content[:start_index + len(start_marker)] + '\n' + image_html + html_content[end_index:]
     else:
@@ -44,11 +57,63 @@ def update_file(images_folder, index_file):
             f'<div id="image-container">\n{image_html}</div>\n</body>'
         )
 
-    # Write the updated HTML content back to the index.html file
     with open(index_file, 'w') as file:
         file.write(new_html_content)
 
     print(f"{index_file} has been updated successfully.")
 
-update_file('feed', './index.html')
-update_file('feed', './feed.html')
+update_file(images_folder, './index.html')
+update_file(images_folder, './feed.html')
+
+
+
+
+
+
+
+
+
+# import os
+# from datetime import datetime
+
+# images_folder = 'feed'
+# index_file = './index.html'
+
+# def update_file(images_folder, index_file):
+#     image_files = sorted(
+#         [f for f in os.listdir(images_folder) if f.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))],
+#         key=lambda x: os.path.getctime(os.path.join(images_folder, x)),
+#         reverse=True
+#     )
+
+#     image_html = ""
+#     for image_file in image_files:
+#         img_src = os.path.join(images_folder, image_file)
+#         filename = os.path.splitext(image_file)[0].replace('-', ' ')
+#         creation_time = datetime.fromtimestamp(os.path.getctime(os.path.join(images_folder, image_file)))
+#         formatted_date = creation_time.strftime('%m/%d/%y')
+#         image_html += f'<img class="feed-image" src="{img_src}">\n<p class="feed-text">{image_file}</p>\n'
+
+#     with open(index_file, 'r') as file:
+#         html_content = file.read()
+
+#     start_marker = '<div id="image-container">'
+#     end_marker = '</div>'
+#     start_index = html_content.find(start_marker)
+#     end_index = html_content.find(end_marker, start_index)
+
+#     if start_index != -1 and end_index != -1:
+#         new_html_content = html_content[:start_index + len(start_marker)] + '\n' + image_html + html_content[end_index:]
+#     else:
+#         new_html_content = html_content.replace(
+#             '</body>',
+#             f'<div id="image-container">\n{image_html}</div>\n</body>'
+#         )
+
+#     with open(index_file, 'w') as file:
+#         file.write(new_html_content)
+
+#     print(f"{index_file} has been updated successfully.")
+
+# update_file('feed', './index.html')
+# update_file('feed', './feed.html')
